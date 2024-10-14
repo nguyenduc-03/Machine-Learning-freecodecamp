7edf0d527703463b64e2faeebd7180133bf85f85
Nguyen Duc <thaiduc0706@gmail.com>
2024-10-14 13:26:23 +0200

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,10 +1,8 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
-my_history = []
-init_play = prev_play = 'S'
-opponent_list = [False, False, False, False]
-ideal_response = {'P' : 'R', 'R' : 'S', 'S' : 'P'}
-opponent_quincy_counter = -1
+player_history= []
+first_play = prev_player_move = 'S'
+bot_list = [ False, False, False,False]
+counter = [0]
+counter_response = { 'P' : 'R', 'R' : 'S', 'S' : 'P'}
 play_order = [{
     "RR" : 0, 
     "RP" : 0, 
@@ -17,45 +15,45 @@ play_order = [{
     "SS" : 0, 
     }]
 
-def player(prev_opponent_play, opponent_history = []):
-    global my_history, prev_play, opponent_list, ideal_response, opponent_quincy_counter, play_order
+def player ( prev_opponent_play, opponent_history = []):
+    global player_history, prev_player_move, bot_list, counter_response, play_order
     opponent_history.append(prev_opponent_play)
-    my_history.append(prev_play)
+    player_history.append(prev_player_move)
 
-    # quincy
-    if(len(set(opponent_list)) == 1 and opponent_history[-5:] == ['R', 'P', 'P', 'S', 'R']):
-        opponent_list[0] = True
-
-    if(opponent_list[0]):
-        if(len(opponent_history) % 1000 == 0):
-            opponent_list = [False, False, False, False]
-            opponent_history.clear()
-            
-        opponent_quincy_list = ['P', 'S', 'S', 'R', 'P'] 
-        opponent_quincy_counter = (opponent_quincy_counter + 1) % 5
-        return opponent_quincy_list[opponent_quincy_counter]
+    #quincy
+    if(len(set(bot_list)) == 1 and opponent_history[-5:] == [ 'R', 'P', 'P', 'S', 'R']):
+        bot_list[0] = True
     
-    # abbey
-    if(len(set(opponent_list)) == 1 and opponent_history[-5:] == ['P', 'P', 'R', 'R', 'R']):
-        opponent_list[1] = True
+    if(bot_list[0]):
+        if(len(opponent_history) % 1000 == 0):
+            bot_list = [False, False, False,False]
+            opponent_history.clear()
 
-    if(opponent_list[1]): 
-        last_two = ''.join(my_history[-2:])
+        counter[0] += 1
+        choices = ["P", "P", "S", "S", "R"]
+        return choices[counter[0] % len(choices)]
+
+    #abbey and mrugesh
+    if(len(set(bot_list)) == 1  and opponent_history[-5:] == ['P', 'P','R','R','R']):
+        bot_list[1]= True
+
+    if(bot_list[1]):
+        last_two = ''.join(player_history[-2:])
         if(len(last_two) == 2):
             play_order[0][last_two] += 1
         potential_plays = [
-            prev_play + 'R', 
-            prev_play + 'P', 
-            prev_play + 'S', 
+            prev_player_move + 'R', 
+            prev_player_move + 'P',
+            prev_player_move + 'S',
             ]
         sub_order = {
-            k : play_order[0][k]
+            k: play_order[0][k]
             for k in potential_plays if k in play_order[0]
             }
-        prediction = max(sub_order, key = sub_order.get)[-1:]
-        
+        prediction = max(sub_order, key=sub_order.get)[-1:]
+
         if(len(opponent_history) % 1000 == 0):
-            opponent_list = [False, False, False, False]
+            bot_list = [False, False, False, False]
             opponent_history.clear()
             play_order = [{
               "RR" : 0, 
@@ -68,35 +66,18 @@ def player(prev_opponent_play, opponent_history = []):
               "SP" : 0, 
               "SS" : 0, 
               }]
+        prev_player_move = counter_response[prediction]
+        return counter_response[prediction]
 
-        prev_play = ideal_response[prediction]
-        return prev_play
+    #kris
+    if(len(set(bot_list)) == 1  and opponent_history[-5:] == ['P', 'R','R','R','R']):
+        bot_list[2]= True
 
-    # kris
-    if(len(set(opponent_list)) == 1 and opponent_history[-5:] == ['P', 'R', 'R', 'R', 'R']):
-        opponent_list[2] = True
+    if(bot_list[2]):
+        if prev_player_move == '':
+            prev_player_move = "S"
+        prev_player_move = counter_response[prev_player_move]
+        return prev_player_move
 
-    if(opponent_list[2]):
-        if(len(opponent_history) % 1000 == 0):
-            opponent_list = [False, False, False, False]
-            opponent_history.clear()
-            
-        prev_play = ideal_response[prev_play]
-        return prev_play
+    return prev_player_move
 
-    # mrugesh
-    if(len(set(opponent_list)) == 1 and opponent_history[-5:] == ['R', 'R', 'R', 'R', 'R']):
-        opponent_list[3] = True
-    
-    if(opponent_list[3]):  
-        if(len(opponent_history) == 1000):
-            opponent_list = [False, False, False, False]
-            opponent_history.clear()
-            
-        last_ten = my_history[-10:]
-        most_frequent = max(set(last_ten), key = last_ten.count)
-        prev_play = ideal_response[most_frequent]
-        return prev_play
-    
-    prev_play = init_play
-    return prev_play
